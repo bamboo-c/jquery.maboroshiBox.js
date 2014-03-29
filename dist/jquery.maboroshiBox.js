73b7b13e6f23de4095d3cb19f4e30ae825c9e92b
isoda yu <isoda@maboroshi.biz>
2014-03-29 11:44:18 +0900

    プラグイン初期設定 #2

--- dist/jquery.maboroshiBox.js
+++ dist/jquery.maboroshiBox.js
@@ -3,4 +3,27 @@
  *  MIT-style license.
  *  2014 maboroshi, inc.
  */
-console.log("hoge");
+
+(function($, window, undefined) {
+  'use strict';
+  // コンストラクタ定義
+  var MaboroshiBox = function () {
+  };
+
+  // メソッド定義
+  MaboroshiBox.prototype = {
+  };
+
+  // maboroshiBox定義
+  $.fn.maboroshiBox = function (options) {
+    // デフォルトオプションをユーザー定義で上書き
+    options = $.extend({}, $.fn.maboroshiBox.defaults, options);
+
+    return this.each(function (i, el) {
+    });
+  };
+
+  // デフォルトオプション定義
+  $.fn.maboroshiBox.defaults = {
+  };
+})(jQuery, this);
